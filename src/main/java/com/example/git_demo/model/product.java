1728dac1497127739ecc3513cedf480b7068db30
chandanaj <jagadishchandana1@gmail.com>
2025-02-15 17:18:54 +0530

    create the new entity -product

---
+++ src/main/java/com/example/git_demo/model/product.java
@@ -0,0 +1,32 @@
+package com.example.git_demo.model;
+
+public class product {
+    private int Id;
+    private String name;
+    private String email'
+        '
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
--- src/main/java/com/example/git_demo/user/User1.java
+++ src/main/java/com/example/git_demo/user/User1.java
@@ -1,4 +1,43 @@
 package com.example.git_demo.user;
 
 public class User1 {
+    int Id;
+    String Username;
+    String Password;
+    String Email;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
 }
+
+
